5e6cb1acdb9dfb319b968bcd5bc4cbdf6b38f8c6
robert_debauch <robert.debauch@gmail.com>
2025-09-23 13:01:44 +0500

    feat: add the way to display number (up to 10 digits) in display and store it inside variable.

--- main.js
+++ main.js
@@ -1,83 +1,101 @@
+
+// default values for all calculation variables
+const DEFAULT_NUMBER = "";
+const DEFAULT_OPERATOR = "";
+
+// variables for inputs
+let numberOne = DEFAULT_NUMBER;
+let numberTwo = DEFAULT_NUMBER;
+let operator = DEFAULT_OPERATOR;
+
+// let calculationData = []; for future use?
+
+const display = document.querySelector("#display");
+const defaultDisplayValue = "0";
+
+
 const add = (numOne, numTwo) => {
-    let sum = 0 || null;
-    return sum = numOne + numTwo;
+    return numOne + numTwo;
 }
 
-const subtract = (numOne, numTwo, operator) => {
-    let sum = 0 || null;
-    return sum = numOne - numTwo;
+const subtract = (numOne, numTwo) => {
+    return numOne - numTwo;
 }
 
-const multiply = (numOne, numTwo, operator) => {
-    let sum = 0 || null;
-    return sum = numOne * numTwo;
+const multiply = (numOne, numTwo) => {
+    return numOne * numTwo;
 }
 
-const divide = (numOne, numTwo, operator) => {
-    let sum = 0 || null;
-    return sum = numOne / numTwo;
+const divide = (numOne, numTwo) => {
+    return numOne / numTwo;
 }
-
-// function with the call to math operations
 
 const operate = (numOne, numTwo, operator) => {
+
+    let result = 0;
+
     if (operator === "+") {
-        return add(numOne, numTwo, operator);
+        result = add(numOne, numTwo);
     }
     if (operator === "-") {
-        return subtract(numOne, numTwo, operator)
+        result = subtract(numOne, numTwo);
     }
     if (operator === "*") {
-        return multiply(numOne, numTwo, operator)
+        result = multiply(numOne, numTwo);
     }
     if (operator === "/") {
         if (numTwo === 0) {
-            let display = document.querySelector('#result');
             display.textContent = 'BAD! BAD!';
-            return null; // should do something else.
+            return;
         }
-        return divide(numOne, numTwo, operator)
+        result = divide(numOne, numTwo);
     }
+    return result;
 }
 
-// console.log(operate(10, 5, "*"));
+/*  LATER CASE OF USING:
+    let calculationOutput = operate(numberOne, numberTwo, operator);
+    if (calculationOutput !== undefined) {
+    display.textContent = calculationOutput;
+    }
+*/
 
+// ALL NUMBER BUTTONS SELECTION
+const numbersButton = document.querySelectorAll(".number");
+numbersButton.forEach((number) => {
+    number.addEventListener('click', () => {
+        let buttonValue = number.textContent;
 
+        if (display.textContent === defaultDisplayValue) {
+            display.textContent = "";
+            display.textContent += buttonValue;
+            numberOne = parseFloat(display.textContent);
+        }
+        else {
+            if (display.textContent.length <= 10) {
+                display.textContent += buttonValue;
+                numberOne = parseFloat(display.textContent);
+            } else {
+                return;
+            }
+        }
 
-// select display
-const display = document.querySelector('#displayInfo');
-console.log(display);
+        console.log(numberOne);
 
-// select buttons
-const keyOne = document.querySelector("#one");
-keyOne.addEventListener('click', () => {
-    console.log('key one is pressed');
-    display.textContent = keyOne.textContent;
+    })
+    console.log(numberOne);
+    return numberOne;
 });
 
-// for every key -> but we should't select clear, equals, del. 
-const keys = document.querySelectorAll('.key');
-keys.forEach((key) => key.addEventListener('click', () => {
-    alert(`key is pressed`);
-    console.log('key is pressed');
-    display.textContent = key.textContent;
-}));
+// DISPLAY CLEARING + VARIABLES RESETING
+const clearEverything = () => {
+    // displayData = [];
+    display.textContent = defaultDisplayValue;
+    numberOne = DEFAULT_NUMBER;
+    numberTwo = DEFAULT_NUMBER;
+    operator = DEFAULT_OPERATOR;
+    console.clear(); // just for my convenience
+}
 
-
-
-
-
-
-
-
-
-
-
-/* check various stuff 
-let resulting = operate(10, 5, "/");
-console.log(typeof resulting);
-
-// quick check
-let display = document.querySelector('#result');
-display.textContent = operate(132, 2342342, "+");
-*/ 
+const clearButton = document.querySelector("#clear");
+clearButton.addEventListener('click', clearEverything);
