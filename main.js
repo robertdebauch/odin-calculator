b38544cc2c0e7b6617f44a963c52e94f9f2e78f0
robert_debauch <robert.debauch@gmail.com>
2025-09-25 12:45:17 +0500

    feat: add the functionality to perform calculations repeatedly.

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ const DEFAULT_OPERATOR = "";
 let numberOne = DEFAULT_NUMBER;
 let numberTwo = DEFAULT_NUMBER;
 let operator = DEFAULT_OPERATOR;
-
-// let calculationData = []; for future use?
+let enteringSecondNumber = false;
+let calculationOutput = 0;
 
 const display = document.querySelector("#display");
 const defaultDisplayValue = "0";
@@ -37,13 +37,13 @@ const operate = (numOne, numTwo, operator) => {
     if (operator === "+") {
         result = add(numOne, numTwo);
     }
-    if (operator === "-") {
+    if (operator === "−") {
         result = subtract(numOne, numTwo);
     }
-    if (operator === "*") {
+    if (operator === "×") {
         result = multiply(numOne, numTwo);
     }
-    if (operator === "/") {
+    if (operator === "÷") {
         if (numTwo === 0) {
             display.textContent = 'BAD! BAD!';
             return;
@@ -53,39 +53,110 @@ const operate = (numOne, numTwo, operator) => {
     return result;
 }
 
-/*  LATER CASE OF USING:
-    let calculationOutput = operate(numberOne, numberTwo, operator);
-    if (calculationOutput !== undefined) {
-    display.textContent = calculationOutput;
-    }
-*/
-
 // ALL NUMBER BUTTONS SELECTION
-const numbersButton = document.querySelectorAll(".number");
-numbersButton.forEach((number) => {
-    number.addEventListener('click', () => {
-        let buttonValue = number.textContent;
+const numbersButtons = document.querySelectorAll(".number");
 
-        if (display.textContent === defaultDisplayValue) {
-            display.textContent = "";
-            display.textContent += buttonValue;
-            numberOne = parseFloat(display.textContent);
-        }
-        else {
-            if (display.textContent.length <= 10) {
-                display.textContent += buttonValue;
-                numberOne = parseFloat(display.textContent);
-            } else {
-                return;
+const chooseNumber = () => {
+    numbersButtons.forEach((number) => {
+        number.addEventListener('click', () => {
+            let buttonValue = number.textContent;
+            // IF operator is NOT chosen yet - then we select numberOne
+            if (operator === DEFAULT_OPERATOR) {
+
+                if (display.textContent === defaultDisplayValue) {
+                    display.textContent = "";
+                    display.textContent += buttonValue;
+                    numberOne = parseFloat(display.textContent);
+                }
+
+                else {
+                    if (display.textContent.length <= 10) {
+                        display.textContent += buttonValue;
+                        numberOne = parseFloat(display.textContent);
+                    } else {
+                        return;
+                    }
+                }
+                console.log(numberOne + " it's number one");
+                return numberOne;
+
+                // ELSE (operator is chose) - select numberTwo
+            } else if (operator !== DEFAULT_OPERATOR) {
+
+                // switching to the second number
+                if (enteringSecondNumber) {
+                    display.textContent = "";
+                    enteringSecondNumber = false;
+                }
+
+                if (display.textContent === defaultDisplayValue) {
+                    // display.textContent = "";
+                    display.textContent += buttonValue;
+                    numberTwo = parseFloat(display.textContent);
+                }
+
+                else {
+                    if (display.textContent.length <= 10) {
+                        display.textContent += buttonValue;
+                        numberTwo = parseFloat(display.textContent);
+                    } else {
+                        return;
+                    }
+                }
             }
-        }
+            console.log(numberTwo + " it's number two");
+            return numberTwo;
 
+        });
+    });
+
+};
+
+chooseNumber();
+
+// EQUALS BUTTON SELECTION
+// only blue print for now;
+const equalsButton = document.querySelector('#equal');
+equalsButton.addEventListener('click', () => {
+    if (numberOne != DEFAULT_NUMBER && numberTwo != DEFAULT_NUMBER && operator != DEFAULT_OPERATOR) {
+        calculationOutput = operate(numberOne, numberTwo, operator).toFixed(4);
+        numberOne = parseFloat(calculationOutput);
+        numberTwo = DEFAULT_NUMBER;
+        cleanOperator();
+        display.textContent = numberOne;
         console.log(numberOne);
-
-    })
-    console.log(numberOne);
-    return numberOne;
+    } else {
+        return;
+    }
 });
+
+
+// ALL OPERATOR BUTTONS SELECTION
+const operatorsButton = document.querySelectorAll(".operator");
+
+operatorsButton.forEach((op) => {
+    op.addEventListener('click', () => {
+        if (display.textContent !== defaultDisplayValue) {
+            let buttonValue = op.textContent;
+            operator = buttonValue;
+            enteringSecondNumber = true;
+
+            operatorsButton.forEach((button) => {
+                button.classList.remove('highlight');
+            });
+
+            op.classList.toggle('highlight');
+            console.log(operator);
+
+        } else {
+
+            return;
+        }
+    });
+
+    return operator;
+});
+
 
 // DISPLAY CLEARING + VARIABLES RESETING
 const clearEverything = () => {
@@ -93,9 +164,18 @@ const clearEverything = () => {
     display.textContent = defaultDisplayValue;
     numberOne = DEFAULT_NUMBER;
     numberTwo = DEFAULT_NUMBER;
-    operator = DEFAULT_OPERATOR;
+
+    cleanOperator();
+
     console.clear(); // just for my convenience
 }
 
 const clearButton = document.querySelector("#clear");
 clearButton.addEventListener('click', clearEverything);
+
+const cleanOperator = () => {
+    operator = DEFAULT_OPERATOR;
+    operatorsButton.forEach((button) => {
+        button.classList.remove('highlight');
+    });
+}
