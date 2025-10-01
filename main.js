7d42634ca13e80b69f079ced1f831f1f8ee1735a
robert_debauch <robert.debauch@gmail.com>
2025-10-01 15:39:03 +0500

    feat: add error checking for the case of dividing by zero

--- main.js
+++ main.js
@@ -1,18 +1,58 @@
 
 // default values for all calculation variables
-const DEFAULT_NUMBER = "";
+const DEFAULT_NUMBER = null;
 const DEFAULT_OPERATOR = "";
 
 // variables for inputs
 let numberOne = DEFAULT_NUMBER;
 let numberTwo = DEFAULT_NUMBER;
 let operator = DEFAULT_OPERATOR;
-let enteringSecondNumber = false;
+let enteringSecondNumber = false; // this is using to indicate what number user is entering at the moment.
+let isError = false; // this is using when the user is dividing by zero. 
 let calculationOutput = 0;
+let storeCalculationOutput;
+
+const clearButton = document.querySelector("#clear");
+const deleteButton = document.querySelector("#delete");
 
 const display = document.querySelector("#display");
-const defaultDisplayValue = "0";
+const defaultDisplayValue = "TYPE NUMBERS";
 
+
+
+// doesn't work as intended -> can't achieve complete deletion of numberTwo and return to the calculation result
+deleteButton.addEventListener('click', () => {
+    // somewhere here is a mistake: we still somehow deleting numberOne values;
+    // if display is empty - reset to default value
+    if (display.textContent === defaultDisplayValue || display.textContent.length === 1) {
+        display.textContent = "";
+
+    } else if (display.textContent === storeCalculationOutput.toString()) {
+        display.textContent = storeCalculationOutput;
+
+        if (numberTwo === DEFAULT_NUMBER) {
+            display.textContent = storeCalculationOutput.toString();
+
+            numberOne = storeCalculationOutput;
+            numberTwo = DEFAULT_NUMBER;
+            // if we deleting after calculating, we should be able to return to the result of the calculation if we delete all numberTwo
+            // and also we shoud reset operator highlght class
+        }
+
+    } else if (display.textContent !== defaultDisplayValue && numberTwo !== DEFAULT_NUMBER) {
+        let deleted = display.textContent.slice(0, -1);
+        if (deleted.length === 0) {
+            numberTwo = DEFAULT_NUMBER;
+            return;
+        }
+
+        numberTwo = parseFloat(deleted);
+        console.log(deleted);
+        display.textContent = deleted;
+    }
+});
+
+// MATH
 
 const add = (numOne, numTwo) => {
     return numOne + numTwo;
@@ -30,23 +70,24 @@ const divide = (numOne, numTwo) => {
     return numOne / numTwo;
 }
 
+// OPERATION HANDLER
+
 const operate = (numOne, numTwo, operator) => {
 
-    let result = 0;
+    let result;
 
     if (operator === "+") {
         result = add(numOne, numTwo);
     }
-    if (operator === "−") {
+    else if (operator === "−") {
         result = subtract(numOne, numTwo);
     }
-    if (operator === "×") {
+    else if (operator === "×") {
         result = multiply(numOne, numTwo);
     }
-    if (operator === "÷") {
+    else if (operator === "÷") {
         if (numTwo === 0) {
-            display.textContent = 'BAD! BAD!';
-            return;
+            return null;
         }
         result = divide(numOne, numTwo);
     }
@@ -56,11 +97,17 @@ const operate = (numOne, numTwo, operator) => {
 // ALL NUMBER BUTTONS SELECTION
 const numbersButtons = document.querySelectorAll(".number");
 
+// function to choose the value of numberOne or numberTwo
 const chooseNumber = () => {
     numbersButtons.forEach((number) => {
         number.addEventListener('click', () => {
+            if (isError) { clearEverything(); }
             let buttonValue = number.textContent;
-            // IF operator is NOT chosen yet - then we select numberOne
+
+            if (numberOne === storeCalculationOutput && numberTwo == DEFAULT_NUMBER && operator == DEFAULT_OPERATOR) {
+                clearEverything();
+            }
+
             if (operator === DEFAULT_OPERATOR) {
 
                 if (display.textContent === defaultDisplayValue) {
@@ -70,17 +117,17 @@ const chooseNumber = () => {
                 }
 
                 else {
-                    if (display.textContent.length <= 10) {
+                    if (display.textContent.length <= 6) {
                         display.textContent += buttonValue;
                         numberOne = parseFloat(display.textContent);
                     } else {
                         return;
                     }
                 }
-                console.log(numberOne + " it's number one");
+                console.log(numberOne);
                 return numberOne;
 
-                // ELSE (operator is chose) - select numberTwo
+                // ELSE (operator is chosen) - select numberTwo
             } else if (operator !== DEFAULT_OPERATOR) {
 
                 // switching to the second number
@@ -96,7 +143,7 @@ const chooseNumber = () => {
                 }
 
                 else {
-                    if (display.textContent.length <= 10) {
+                    if (display.textContent.length <= 6) {
                         display.textContent += buttonValue;
                         numberTwo = parseFloat(display.textContent);
                     } else {
@@ -104,8 +151,10 @@ const chooseNumber = () => {
                     }
                 }
             }
-            console.log(numberTwo + " it's number two");
+
+            console.log(numberTwo);
             return numberTwo;
+
 
         });
     });
@@ -114,29 +163,20 @@ const chooseNumber = () => {
 
 chooseNumber();
 
-// EQUALS BUTTON SELECTION
-// only blue print for now;
-const equalsButton = document.querySelector('#equal');
-equalsButton.addEventListener('click', () => {
-    if (numberOne != DEFAULT_NUMBER && numberTwo != DEFAULT_NUMBER && operator != DEFAULT_OPERATOR) {
-        calculationOutput = operate(numberOne, numberTwo, operator).toFixed(4);
-        numberOne = parseFloat(calculationOutput);
-        numberTwo = DEFAULT_NUMBER;
-        cleanOperator();
-        display.textContent = numberOne;
-        console.log(numberOne);
-    } else {
-        return;
-    }
-});
-
 
 // ALL OPERATOR BUTTONS SELECTION
 const operatorsButton = document.querySelectorAll(".operator");
 
 operatorsButton.forEach((op) => {
     op.addEventListener('click', () => {
+        if (isError) { clearEverything(); }
         if (display.textContent !== defaultDisplayValue) {
+
+            // logic for chaining operations
+            if (numberOne != DEFAULT_NUMBER && numberTwo != DEFAULT_NUMBER && operator != DEFAULT_OPERATOR) {
+                resultOfOperation();
+            }
+
             let buttonValue = op.textContent;
             operator = buttonValue;
             enteringSecondNumber = true;
@@ -149,31 +189,59 @@ operatorsButton.forEach((op) => {
             console.log(operator);
 
         } else {
-
             return;
         }
     });
 
-    return operator;
 });
 
+const resultOfOperation = () => {
+
+    let result = operate(numberOne, numberTwo, operator);
+
+    if (result === null) {
+        display.textContent = 'BAD! BAD!';
+        isError = true;
+        clearOperator();
+        numberOne = DEFAULT_NUMBER;
+        numberTwo = DEFAULT_NUMBER;
+        return;
+    }
+
+    calculationOutput = Number(result.toFixed(4));
+
+    // storeCalculationOutput = Math.trunc(calculationOutput); come back to it when you will do delete button
+    numberOne = calculationOutput;
+    numberTwo = DEFAULT_NUMBER;
+    clearOperator();
+    display.textContent = numberOne;
+}
+
+const equalsButton = document.querySelector('#equal');
+equalsButton.addEventListener('click', () => {
+    if (isError) { clearEverything(); }
+    if (numberOne != DEFAULT_NUMBER && numberTwo != DEFAULT_NUMBER && operator != DEFAULT_OPERATOR) {
+        resultOfOperation();
+    } else {
+        return;
+    }
+});
 
 // DISPLAY CLEARING + VARIABLES RESETING
 const clearEverything = () => {
-    // displayData = [];
+    isError = false;
     display.textContent = defaultDisplayValue;
     numberOne = DEFAULT_NUMBER;
     numberTwo = DEFAULT_NUMBER;
 
-    cleanOperator();
-
-    console.clear(); // just for my convenience
+    clearOperator();
+    console.clear();
 }
 
-const clearButton = document.querySelector("#clear");
+
 clearButton.addEventListener('click', clearEverything);
 
-const cleanOperator = () => {
+const clearOperator = () => {
     operator = DEFAULT_OPERATOR;
     operatorsButton.forEach((button) => {
         button.classList.remove('highlight');
